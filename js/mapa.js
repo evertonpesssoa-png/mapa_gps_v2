116c85a1aab944e92cad7c7e372199824b243a60
Everton Pessoa <evertonpesssoa@gmail.com>
2026-01-19 21:50:16 -0300

    Atualizar o mapa.js

--- js/mapa.js
+++ js/mapa.js
@@ -1,48 +1,64 @@
-let map = L.map('map').setView([-23.55, -46.63], 14);
+document.addEventListener("DOMContentLoaded", () => {
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 19
-}).addTo(map);
+  const map = L.map('map').setView([-23.55, -46.63], 14);
 
-let userMarker = null;
-let userCircle = null;
-let poiLayer = L.layerGroup().addTo(map);
-let poisLoaded = false;
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19
+  }).addTo(map);
 
-function startGPS() {
-  if (!navigator.geolocation) {
-    alert("Geolocalização não suportada");
-    return;
-  }
+  let userMarker = null;
+  let userCircle = null;
+  const poiLayer = L.layerGroup().addTo(map);
+  let poisLoaded = false;
 
-  navigator.geolocation.watchPosition(pos => {
-    const { latitude, longitude, accuracy } = pos.coords;
-
-    if (!userMarker) {
-      userMarker = L.marker([latitude, longitude]).addTo(map);
-      userCircle = L.circle([latitude, longitude], {
-        radius: accuracy,
-        color: '#0096ff',
-        fillOpacity: 0.3
-      }).addTo(map);
-      map.setView([latitude, longitude], 16);
-    } else {
-      userMarker.setLatLng([latitude, longitude]);
-      userCircle.setLatLng([latitude, longitude]);
+  function startGPS() {
+    if (!navigator.geolocation) {
+      alert("Geolocalização não suportada");
+      return;
     }
 
-    if (!poisLoaded) {
-      //loadAutoPOIs(latitude, longitude, 1200);
-      //loadManualPOIs();
-      poisLoaded = true;
-    }
-  });
-}
+    navigator.geolocation.watchPosition(
+      pos => {
+        const { latitude, longitude, accuracy } = pos.coords;
 
-function centerOnUser() {
-  if (userMarker) {
-    map.setView(userMarker.getLatLng(), 16);
+        if (!userMarker) {
+          userMarker = L.marker([latitude, longitude]).addTo(map);
+          userCircle = L.circle([latitude, longitude], {
+            radius: accuracy,
+            color: '#0096ff',
+            fillOpacity: 0.3
+          }).addTo(map);
+
+          map.setView([latitude, longitude], 16);
+        } else {
+          userMarker.setLatLng([latitude, longitude]);
+          userCircle.setLatLng([latitude, longitude]);
+        }
+
+        if (!poisLoaded) {
+          loadManualPOIs(poiLayer);
+          if (navigator.onLine) {
+            loadAutoPOIs(latitude, longitude, 1200, poiLayer);
+          }
+          poisLoaded = true;
+        }
+      },
+      err => {
+        alert("Erro GPS: " + err.message);
+        console.error(err);
+      },
+      { enableHighAccuracy: true }
+    );
   }
-}
 
-startGPS();
+  function centerOnUser() {
+    if (userMarker) {
+      map.setView(userMarker.getLatLng(), 16);
+    }
+  }
+
+  // expõe globalmente (botão + compatibilidade total)
+  window.centerOnUser = centerOnUser;
+
+  startGPS();
+});
